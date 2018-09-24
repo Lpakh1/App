fad0e17915906274b619598c94a93b93a36c7c88
LUQMAN AKHTAR <lpakh@hotmail.com>
2018-09-24 22:33:12 +0200

    First Functionality Done
    - Added the layouts.
    - Listed the products.
    - Need more comments and Design.

--- myApp/App.js
+++ myApp/App.js
@@ -1,15 +1,156 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button,Alert } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  
+  //state = {myState: 'The text can be changed'}
+  state = { dataS: '',
+            isLoading : true,
+          }
+
+  
+
+//Fucntion
+//updateState = () => this.setState({myState: 'New Data has arrived' })  
+
+
+  //Get Request 
+  componentDidMount = () => {
+      fetch('https://services.odata.org/V2/(S(1pvfcxajki3ouml43k5wnex3))/OData/OData.svc/Products?$format=json', {
+         method: 'GET',
+         'Content-Type': 'application/json'
+
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+         console.log(responseJson.d);
+         
+         this.setState({
+            dataS: responseJson.d,
+            isLoading: false,
+         })
+      })
+      .catch((error) => {
+         console.error(error);
+      });
+   }
+
+
+
+
+
+
+
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+   
+
+
+   if(this.state.isLoading) {
+       return (
+             
+             <View style={styles.container}>
+                 <Text>Please wait </Text>
+             </View>
+              )
+
+}  else {
+
+     let products = this.state.dataS.map((val, key)=>{
+         
+// Seems like someone is already maniputaling the API :)
+
+if(val.Name!= "TEEEESSSTT")
+    {
+
+      
+         return <View key={key} style={styles.item} >
+            
+    
+          <Text style={{fontWeight: "bold",fontSize: 13,}} > {val.Name} </Text>
+        
+        </View>
+
   }
+
+       });
+
+
+
+
+
+    return (
+      
+
+     // Listing the products on the screen
+            
+ <View style={{flex: 1,}}>  
+
+       <View style={{flex: 1, flexDirection: 'row',alignItems: 'flex-start',flexWrap: 'wrap',justifyContent: 'space-between', margin: 50 }}>
+          
+              {products}
+              
+            </View>
+
+
+
+
+  <View>
+
+        <Button onPress={() => {
+                alert("I am pressed")
+              }}
+              title="Press To ADD Products"/>
+
+
+
+  </View>
+
+
+</View>
+
+//<Button
+//  onPress={() => {
+//    Alert.alert('You tapped the button!');
+//  }}
+//  title="Press Me"/>
+
+
+//      </View>
+   );
+
+
+
+
+
+
+
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+}
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +159,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+item: {
+ 
+    
+    height : 100,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+ 
+}
+
+
 });
